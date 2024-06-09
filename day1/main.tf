1849edef3f429db9d56e4c17f781674288585556
MUSKAN <upasanadamahe12@gmail.com>
2024-06-09 18:08:09 +0530

    new

--- day1/main.tf
+++ day1/main.tf
@@ -1,12 +1,10 @@
-provider "aws"{
- region ="us-east-1"
-}
+
 resource "aws_instance" "name" {
  ami = "ami-0bb84b8ffd87024d8"
  instance_type = "t2.micro"
  key_name = "mykey"
  tags = {
-   Name = "My-server"
+   Name = "My-server-1"
  }
   
 }
